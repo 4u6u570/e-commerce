f28661a903a12744ec4bac0d6999f7864890b7f0
Cristian Bejarano <kanu_nirvana@hotmail.com>
2020-06-14 01:54:37 -0300

    primeros cambios de estilo

---
+++ maquetacion/js/main.js
@@ -0,0 +1,11 @@
+//variables
+const menuOn= document.getElementById("menu-on");
+
+
+/*menu*/
+menuOn.addEventListener('click', function(){
+document.body.classList.toggle('visible_menu')
+});
+
+
+
