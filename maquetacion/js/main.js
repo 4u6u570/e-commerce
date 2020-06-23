a7ea08d4f4e718ef6ce629fc8601e9173d6bc995
Cristian Bejarano <kanu_nirvana@hotmail.com>
2020-06-22 22:07:19 -0300

    cambios al css

--- maquetacion/js/main.js
+++ maquetacion/js/main.js
@@ -8,8 +8,11 @@ menuOn.addEventListener('click', function(){
 document.body.classList.toggle('visible_menu')
 });
 
-var flicking = new eg.Flicking("#wrapper", {
-    gap: 10,
+var flicking = new eg.Flicking(".flicking", {
     circular: true,
-  });
+    renderOnlyVisible: true,
+    gap: 10,
+});
+
+flicking.addPlugins(new eg.Flicking.plugins.Fade());
 
